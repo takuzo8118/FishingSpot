1aaf4a57306af24bd91f7c136db7dd612c3fa678
takuzo8118 <saitou2930b@gmail.com>
2022-11-12 09:28:13 +0900

    サンプルページのルート追加

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 最初から書いてあるものだが、今後変更する→indexへ飛ばす処理（投稿一覧ページ）
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 // こちらの記述を消してしまうとログインできなくなってしまうため注意が必要
 Auth::routes();
-
+// login機能の実装（laravel/ui）
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
